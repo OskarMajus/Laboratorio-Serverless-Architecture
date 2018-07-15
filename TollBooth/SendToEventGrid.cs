78cb4895abab03eb3a9789d67c38ddd875132630
Oskar <oskarmajus@yahoo.com>
2018-07-15 16:06:14 -0600

    Código de Función Terminado

--- TollBooth/SendToEventGrid.cs
+++ TollBooth/SendToEventGrid.cs
@@ -29,12 +29,12 @@ namespace TollBooth
             if (data.LicensePlateFound)
             {
                 // TODO 3: Modify send method to include the proper eventType name value for saving plate data.
-                // COMPLETE: await Send(...);
+                await Send("SavePlateData", "TollBooth/CustomerService", data);
             }
             else
             {
                 // TODO 4: Modify send method to include the proper eventType name value for queuing plate for manual review.
-                // COMPLETE: await Send(...);
+                await Send("QueuePlateForManualCheckup", "TollBooth/CustomerService", data);
             }
         }
 
